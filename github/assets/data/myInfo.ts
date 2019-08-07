106c61c0099d23404c70c307fc36f890e3eb9bf7
nuganghap <53496833+nuganghap@users.noreply.github.com>
2019-08-07 00:01:02 -0400

    Add files via upload

---
+++ github/assets/data/myInfo.ts
@@ -0,0 +1,13 @@
+import { Nugangha } from 'src/app/nugangha';
+
+// step 2
+export const MYINFO: Nugangha = {
+
+    st_num:'99150055',
+    st_name: 'Patric Nugangha',
+    st_logName: 'nugangha',
+    campus: 'Davis',
+    assig_title:'Assignment4',
+
+};
+    
